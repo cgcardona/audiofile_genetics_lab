dcb940ecc6d80ef9f73abec9dc0d9b746d9d86a1
Carlos Cardona <cgcardona@gmail.com>
2013-01-28 18:24:06 -0800

    stub rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AudiofileGeneticsLab::Application.config.secret_token = '4fa5d67fb71dec40b176d6eeb05e03d1c8bca81a79962506df4ccd0abab8e785c206be17f8282145474bfdfb7a08662ea4554e52ab4887935a365de7b9bf652d'
